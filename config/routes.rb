07196fe47a0ef059612bc9659651010c8d955a95
amin bhs <amin.diez@gmail.com>
2017-08-14 16:56:56 +0200

    ajout des méthodes crud et affichage de index et show sans identification

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,4 @@
 Rails.application.routes.draw do
-  get 'chalets/index'
-
-  get 'chalets/show'
-
-  get 'chalets/new'
-
-  get 'chalets/create'
-
-  get 'chalets/edit'
-
-  get 'chalets/update'
-
-  get 'chalets/destroy'
-
   devise_for :users
   root to: 'chalets#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
