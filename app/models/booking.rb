1492d4b54038e4f5143d12f48f66075d6fd7f740
amin bhs <amin.diez@gmail.com>
2017-08-16 14:44:44 +0200

    ajout de la view booking

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,4 +1,14 @@
 class Booking < ApplicationRecord
   belongs_to :chalet
   belongs_to :user
+
+
+  def during
+    (self.end_date.to_time - self.start_date.to_time).to_i/ 86400
+  end
+
+
+  def before
+    (self.start_date.to_time - Time.now).to_i / 86400
+  end
 end
