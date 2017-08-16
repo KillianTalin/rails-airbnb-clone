f5dc87c77e937f1f3223dda3306e5b9fe37485f9
amin bhs <amin.diez@gmail.com>
2017-08-16 19:17:17 +0200

    push avant de pull

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -7,8 +7,12 @@ class Booking < ApplicationRecord
     (self.end_date.to_time - self.start_date.to_time).to_i/ 86400
   end
 
-
   def before
     (self.start_date.to_time - Time.now).to_i / 86400
   end
+
+  def finish
+(self.end_date.to_time - Time.now).to_i / 86400
+
+end
 end
