fffc712a2a6cccc67965b24883914c4692f2bf13
amin bhs <amin.diez@gmail.com>
2017-08-17 00:30:25 +0200

    ajout de l'index bookings

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,18 +1,39 @@
 class Booking < ApplicationRecord
   belongs_to :chalet
   belongs_to :user
-
+  validates :chalet_id, presence: true
+  validates :user_id, presence: true
 
   def during
     (self.end_date.to_time - self.start_date.to_time).to_i/ 86400
+  end
+
+  def price
+  self.checkout = (during * self.chalet.price).to_f
+  self.save
   end
 
   def before
     (self.start_date.to_time - Time.now).to_i / 86400
   end
 
-  def finish
-(self.end_date.to_time - Time.now).to_i / 86400
+  def finished_ago
+    (Time.now - self.start_date.to_time).to_i / 86400
+  end
 
-end
+  def finish
+    (self.end_date.to_time - Time.now).to_i / 86400
+  end
+
+  def passed
+    (finish < 0) && (before < 0)
+  end
+
+  def futur
+    before > 0
+  end
+
+  def now
+    (finish > 0) && (before < 0)
+  end
 end
