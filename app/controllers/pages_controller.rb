f5b10fe3ca392bfae1716e940eab2bab84bf7880
Killian Talin <killian.talin@live.fr>
2017-08-17 18:59:45 +0200

    Double homepage done

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,6 +3,7 @@ class PagesController < ApplicationController
   def home
     @booking = Booking.new
     @chalet = Chalet.new
+    @selection_mont_blanc = Chalet.where(location: "Massif du Mont-Blanc").limit(3)
+    @selection_beaufortain = Chalet.where(location: "Massif du Beaufortain").limit(3)
   end
-
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -128,7 +128,77 @@ chalets_attributes = [
     user:         User.all.sample,
     last_renovation: "14 janvier 2014",
     photo_urls: ['http://www.refuges-montagne.info/images/argentiere-00.jpg']
-}
+},
+
+  {
+    name:            "Refuge l'Alpage",
+    capacity:        130,
+    city:            "Chamonix-Mont-Blanc",
+    location:        "Massif du Beaufortain",
+    latitude:        45.649597,
+    longitude:       6.564803,
+    altitude:        3613,
+    rating:          3,
+    price:           50,
+    cb_accepted:     true,
+    opening_date:    20170320,
+    closing_date:    20170830,
+    user:         User.all.sample,
+    last_renovation: "14 janvier 2014",
+    photo_urls: ['https://lh3.googleusercontent.com/-MbVZ5BVlaeA/WAInJcEfwCI/AAAAAAAAs5I/ywJ6rZR9Si4bUEvjuh3zxzEHkpqFISyZQCLIB/w645-h320-k-no/']
+  },
+  {
+    name:            "Refuge le Nant du Beurre ",
+    capacity:        120,
+    city:            "Saint-Gervais-les-Bains",
+    location:        "Massif du Beaufortain",
+    latitude:        45.582882,
+    longitude:       6.560423,
+    altitude:        3835,
+    rating:          4,
+    price:           60,
+    cb_accepted:     true,
+    opening_date:    20170320,
+    closing_date:    20170830,
+    user:         User.all.sample,
+    last_renovation: "14 janvier 2014",
+    photo_urls: ['https://www.chamonix.net/sites/default/files/refuge-du-gouter.jpg?itok=uog9XUVO']
+  },
+  {
+    name:            "Refuge de la Croix de fer",
+    capacity:        74,
+    city:            "Saint-Gervais-les-Bains",
+    location:        "Massif du Beaufortain",
+    latitude:        45.795197,
+    longitude:       6.610345,
+    altitude:        3167,
+    rating:          3,
+    price:           100,
+    cb_accepted:     true,
+    opening_date:    20170320,
+    closing_date:    20170830,
+    user:            User.all.sample,
+    last_renovation: "14 janvier 2014",
+    photo_urls: ['http://magdescimes.com/wp-content/uploads/2012/07/25156292452_10151047309892453.jpg']
+
+  },
+  {
+    name:            "Refuge du Col de la Croix",
+    capacity:        100,
+    city:            "Saint-Gervais-les-Bains",
+    location:        "Massif du Beaufortain",
+    latitude:        45.722057,
+    longitude:       6.717266,
+    altitude:        1867,
+    rating:          3,
+    price:           20,
+    cb_accepted:     true,
+    opening_date:    20170320,
+    closing_date:    20170830,
+    user:            User.all.sample,
+    last_renovation: "14 janvier 2014",
+    photo_urls: ['http://www.refuges-montagne.info/images/vallot-00.jpg']
+  }
 ]
 Chalet.create!(chalets_attributes)
 puts 'Finished!'
